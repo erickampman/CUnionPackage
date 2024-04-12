146d6aa1492314e1b73245fa8d27e4e2a9b14408
Eric Kampman <erickampman@yahoo.com>
2024-04-12 07:02:25 -0700

    Neglected to add some files to previous commit.

--- Sources/CUnionPackage/include/MyUnion.h
+++ Sources/CUnionPackage/include/MyUnion.h
@@ -12,7 +12,20 @@
 
 typedef struct MyUnion {
 	UInt32 	uint32NotInUnion;
-	UInt8	uint8ArrayNotInUnion;
+	UInt8	uint8ArrayNotInUnion[8];
+	
+	union {
+		struct {
+			UInt32 asUInt32;
+		} asUInt32;
+		struct {
+			UInt8 b0;
+			UInt8 b1;
+			UInt8 b2;
+			UInt8 b3;
+		} asUInt8;
+	} aUnion;
+	
 } MyUnion;
 
 #endif /* MyUnion_h */
