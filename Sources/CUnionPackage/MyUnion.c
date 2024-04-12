d03cfca20de2bf8b1947491a44ce4c8cb9d804a8
Eric Kampman <erickampman@yahoo.com>
2024-04-12 07:01:52 -0700

    Example of C Struct use in Swift added.

---
+++ Sources/CUnionPackage/MyUnion.c
@@ -0,0 +1,14 @@
+//
+//  MyUnion.c.c
+//  
+//
+//  Created by Eric Kampman on 4/11/24.
+//
+
+#include <MyUnion.h>
+
+
+/*
+ x-xcode-log://3D632922-F8FE-40E0-A989-5B80D410FEF8 target at '/Users/erickampman/Dev-Experiments/CUnionsForSwift/Sources/CUnionsForSwift' contains mixed language source files; feature not supported
+
+ */
---
+++ Sources/CUnionPackage/include/MyUnion.h
@@ -0,0 +1,18 @@
+//
+//  MyUnion.h
+//
+//
+//  Created by Eric Kampman on 4/11/24.
+//
+
+#ifndef MyUnion_h
+#define MyUnion_h
+
+#include <CoreFoundation/CoreFoundation.h>
+
+typedef struct MyUnion {
+	UInt32 	uint32NotInUnion;
+	UInt8	uint8ArrayNotInUnion;
+} MyUnion;
+
+#endif /* MyUnion_h */
